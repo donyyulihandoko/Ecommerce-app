3f30982d9c262a96fbbc67212655712efbc118e7
Dony Yuli Handoko <officialdonyyulihandoko@gmail.com>
2025-12-28 20:44:13 +0700

    commit 2 feature/auth-fortify

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class, 'index'])->middleware('auth');
+Route::get('/', [HomeController::class, 'index'])->middleware('auth')->name('home.index');
